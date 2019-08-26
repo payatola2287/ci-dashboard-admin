b43909d175f655fb91d68a5081a58f0f4eebd12c
Payatola2287 <paolo.j.gallardo@gmail.com>
2019-08-26 08:58:28 +0800

    UPDATE: CodeIgniter Core Update
    CodeIgniter update to 3.1.10

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-
-$config['base_url']     = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url']    .= "://".$_SERVER['HTTP_HOST'];
-$config['base_url']    .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -38,7 +35,7 @@ $config['base_url']    .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SER
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -55,7 +52,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'AUTO';
+$config['uri_protocol']	= 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
